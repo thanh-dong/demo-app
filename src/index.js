09a42e63de5e1a6fa648eb6a9f4dfe7d660ee9af
Hien Cao <hien@the-hash.com>
2017-12-11 18:52:07 +0700

    Setup react route

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
 import Root from './components/Root';
+import {Router, Route, IndexRoute, hashHistory} from "react-router";
+import Archives from "./pages/Archives";
+import Featured from "./pages/Featured";
+import Settings from "./pages/Settings";
 
-render(<Root />, document.getElementById('root'));
+render(<Router history={hashHistory}>
+    <Root/>
+    <IndexRoute component={Featured}></IndexRoute>
+    <Route path="archives(/:article)" name="archives" component={Archives}></Route>
+    <Route path="settings" name="settings" component={Settings}></Route>
+</Router>, document.getElementById('root'));
+
+// render(<Root />, document.getElementById('root'));

a284ee4b0db2b9158b880d57bfc81bc029363285
Thanh Dong <thanh.dqblue@gmail.com>
2017-12-13 12:13:30 +0700

    Revert "Setup react route"
    This reverts commit 09a42e63de5e1a6fa648eb6a9f4dfe7d660ee9af.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import Root from './components/Root';
-import {Router, Route, IndexRoute, hashHistory} from "react-router";
-import Archives from "./pages/Archives";
-import Featured from "./pages/Featured";
-import Settings from "./pages/Settings";
 
-render(<Router history={hashHistory}>
-    <Root/>
-    <IndexRoute component={Featured}></IndexRoute>
-    <Route path="archives(/:article)" name="archives" component={Archives}></Route>
-    <Route path="settings" name="settings" component={Settings}></Route>
-</Router>, document.getElementById('root'));
-
-// render(<Root />, document.getElementById('root'));
+render(<Root />, document.getElementById('root'));

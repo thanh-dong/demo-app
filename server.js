776e4e2f16c9e223fd78563a5ed8b8448ee73551
Thanh Dong <thanh.dqblue@gmail.com>
2017-12-13 15:05:41 +0700

    update

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ import _ from 'lodash'
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (request, response) {
   response.sendFile(__dirname + '/dist/index.html');
 });
